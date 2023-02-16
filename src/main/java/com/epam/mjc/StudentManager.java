cf802a9426a99fa2321f681d602251addbb2a1a7
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-02-16 14:37:52 +0500

    first try

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,15 +5,18 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID){
+    Student found = Student.getValueOf(studentID);
+    if(found == null){
+        throw new StudentNotFoundException("Could not find student with ID " + studentID);
+    } else return found;
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
+        Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
     }
 
